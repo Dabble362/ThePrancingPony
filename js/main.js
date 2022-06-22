b6833486d9e00b2ae7fbd8311d01e4ded7c4e260
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-06-21 23:09:22 -0400

    comment out fetch

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ async function makeReq() {
   document.querySelector("#personOccupation").textContent =
     data.currentOccupation;
 }
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((res) => res.json()) // parse response as JSON
-  .then((data) => {
-    console.log(data.message);
-    document.querySelector("img").src = data.message;
-  })
-  .catch((err) => {
-    console.log(`error ${err}`);
-  });
+// fetch("https://dog.ceo/api/breeds/image/random")
+//   .then((res) => res.json()) // parse response as JSON
+//   .then((data) => {
+//     console.log(data.message);
+//     document.querySelector("img").src = data.message;
+//   })
+//   .catch((err) => {
+//     console.log(`error ${err}`);
+//   });
