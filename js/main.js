aab7d24840b024c59b53e16ca44c8523beb2b7cc
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-07-03 10:14:19 -0400

    add storeName, name value

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-document.querySelector("#astrologer").addEventListener("click", storeCharName);
-document.querySelector("#vagabond").addEventListener("click", storeCharName);
-document.querySelector("#prophet").addEventListener("click", storeCharName);
+document.querySelector("#astrologer").addEventListener("click", storeName);
+document.querySelector("#vagabond").addEventListener("click", storeName);
+document.querySelector("#prophet").addEventListener("click", storeName);
 
-async function makeReq() {
-  const userName = document.querySelector("#charName").value;
-  const res = await fetch(`/api?student=${userName}`);
-  const data = await res.json();
-
-  console.log(data);
-  document.querySelector("#personName").textContent = data.name;
-  document.querySelector("#personStatus").textContent = data.status;
-  document.querySelector("#personOccupation").textContent =
-    data.currentOccupation;
+function storeName() {
+  const userName = document.getElementById("charName").value;
+  console.log(userName);
+  return userName;
 }
+
+// async function makeReq() {
+//   const userName = document.querySelector("#charName").value;
+//   const res = await fetch(`/api?student=${userName}`);
+//   const data = await res.json();
+
+//   console.log(data);
+//   document.querySelector("#personName").textContent = data.name;
+//   document.querySelector("#personStatus").textContent = data.status;
+//   document.querySelector("#personOccupation").textContent =
+//     data.currentOccupation;
+// }
 // fetch("https://dog.ceo/api/breeds/image/random")
 //   .then((res) => res.json()) // parse response as JSON
 //   .then((data) => {
