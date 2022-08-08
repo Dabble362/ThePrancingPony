3dedb5ea86aac6c5741ae651eaccfc5e3e910cc7
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-08-08 13:45:43 -0400

    update connection string

--- app.js
+++ app.js
@@ -4,38 +4,58 @@ const app = express();
 app.use(express.static("public"));
 
 const { MongoClient } = require("mongodb");
-
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-}
-
-async function main() {
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-  const uri =
-    "mongodb+srv://dab362:83l0qu3ntD1n0saur@cluster0.fl26cip.mongodb.net/?retryWrites=true&w=majority";
-
-  const client = new MongoClient(uri);
-
+//Connection URI
+const uri =
+  "mongodb+srv://dab362:8EloquentDinosaur@cluster0.fl26cip.mongodb.net/?retryWrites=true&w=majority";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
   try {
-    // Connect to the MongoDB cluster
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-
-    // Make the appropriate DB calls
-    await listDatabases(client);
-  } catch (e) {
-    console.error(e);
+    // Establish and verify connection
+    await client.db("sample_airbnb").command({ ping: 1 });
+    console.log("Connected successfully to server");
   } finally {
+    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+run().catch(console.dir);
 
-main().catch(console.error);
+// const { MongoClient } = require("mongodb");
+
+// async function listDatabases(client) {
+//   databasesList = await client.db().admin().listDatabases();
+
+//   console.log("Databases:");
+//   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+// }
+
+// async function main() {
+//   /**
+//    * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//    * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//    */
+//   const uri =
+//     "mongodb+srv://dab362:83l0qu3ntD1n0saur@cluster0.fl26cip.mongodb.net/?retryWrites=true&w=majority";
+
+//   const client = new MongoClient(uri);
+
+//   try {
+//     // Connect to the MongoDB cluster
+//     await client.connect();
+
+//     // Make the appropriate DB calls
+//     await listDatabases(client);
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// main().catch(console.error);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
