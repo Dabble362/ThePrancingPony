d2e3026b9f6e015c6474eb9a1269fe582e69e345
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-08-08 13:19:42 -0400

    add mongoDB code, install mondgoDB

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const app = express();
 
-
 app.use(express.static("public"));
+
+const { MongoClient } = require("mongodb");
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri =
+    "mongodb+srv://dab362:83l0qu3ntD1n0saur@cluster0.fl26cip.mongodb.net/?retryWrites=true&w=majority";
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
